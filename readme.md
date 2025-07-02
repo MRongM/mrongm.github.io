80c96fa954202e141d1452025383280df975a850
MRongM <1399316847@qq.com>
2025-07-02 15:17:15 +0800

    update

---
+++ readme.md
@@ -0,0 +1,1 @@
+# https://mrongm.github.io
--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-    render() {
-        return (
-            <div>are you ok?</div>
-        )
-    }
-})
